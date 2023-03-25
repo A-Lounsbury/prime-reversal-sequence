df84b627f47bfe89db76407482df42a7fa5123a5
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2023-03-24 20:54:42 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# prime-reversal-sequence
+This script generates a particular [prime reversal sequence](https://www.youtube.com/watch?v=pAMgUB51XZA). 
+
+output: `[1, 0, 0, 0, -2, 2, 0, -6, -6, 6, 0, -4, 4, -10, -14, 10, 4, 14, -30, -42]`
+
+# Plotting with Seaborn
+## Basic Scatterplots
+Scatterplot of 10 numbers:
+![1000 numbers](10.png)
+Scatterplot of 100 numbers:
+![1000 numbers](100.png)
+Scatterplot of 1000 numbers:
+![1000 numbers](1000.png)
+Scatterplot of 10000 numbers:
+![10000 numbers](10000.png)
+Scatterplot of 100,000 numbers:
+![100,000 numbers](100000.png)
+
+## Plotting the Average as $n \to \infty$
+Scatterplot of the first 10 averages:
+![10 averages](average_10.png)
+Scatterplot of the first 100 averages:
+![100 averages](average_100.png)
+Scatterplot of the first 1,000 averages:
+![1000 averages](average_1000.png)
+Scatterplot of the first 10,000 averages:
+![10,000 averages](average_10000.png)
---
+++ prime-reversal-sequence.py
@@ -0,0 +1,197 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def prime(n):
+    if n == 1:
+        return False
+    
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+# converts a decimal number to a list of binary digits
+def decimalToBinary(n):
+    digits = []
+    while n >= 1:
+        digits.append(n % 2)
+        if n % 2 == 1:
+            n = int((n - 1) / 2)
+        elif n % 2 == 0:
+            n = int(n / 2)
+    return digits
+
+def binaryToDecimal(digits):
+    result = 0
+    digits.reverse()
+    for i, d in enumerate(digits):
+        result += d * (2 ** i)
+    return result
+
+def generate_prime_reversal_sequence(n):
+    sequence = []
+    i = 2
+    while len(sequence) < n:
+        if prime(i):
+            binary = decimalToBinary(i)
+            num = binaryToDecimal(binary)
+            sequence.append(i - num)
+        if i == 2:
+            i += 1
+        else:
+            i += 2
+    return sequence
+
+sequence = generate_prime_reversal_sequence(20)
+print(sequence)
+
+# Basic Scatterplots
+n = 10
+sequence = generate_prime_reversal_sequence(n)
+df = pd.DataFrame(sequence, columns=["Number"])
+
+indices = []
+for i in range(n):
+    indices.append(i)
+df['index'] = indices
+
+sns.scatterplot(x="index", y='Number', data=df)
+plt.show()
+
+n = 100
+sequence = generate_prime_reversal_sequence(n)
+df = pd.DataFrame(sequence, columns=["Number"])
+
+indices = []
+for i in range(n):
+    indices.append(i)
+df['index'] = indices
+
+sns.scatterplot(x="index", y='Number', data=df)
+plt.show()
+
+n = 1000
+sequence = generate_prime_reversal_sequence(n)
+df = pd.DataFrame(sequence, columns=["Number"])
+
+indices = []
+for i in range(n):
+    indices.append(i)
+df['index'] = indices
+
+sns.scatterplot(x="index", y='Number', data=df)
+plt.show()
+
+n = 10000
+sequence = generate_prime_reversal_sequence(n)
+df = pd.DataFrame(sequence, columns=["Number"])
+
+indices = []
+for i in range(n):
+    indices.append(i)
+df['index'] = indices
+
+sns.scatterplot(x="index", y='Number', data=df)
+plt.show()
+
+# Average plots
+n = 10
+sequence = generate_prime_reversal_sequence(n)
+
+average_sequence = []
+total = 0
+for i, s in enumerate(sequence):
+    total += s
+    average = total / (i + 1)
+    average_sequence.append(average)
+
+df = pd.DataFrame(average_sequence, columns=["Average"])
+
+indices = []
+for i in range(n):
+    indices.append(i)
+df['index'] = indices
+
+sns.scatterplot(x='index', y='Average', data=df)
+plt.show()
+
+n = 100
+sequence = generate_prime_reversal_sequence(n)
+
+average_sequence = []
+total = 0
+for i, s in enumerate(sequence):
+    total += s
+    average = total / (i + 1)
+    average_sequence.append(average)
+
+df = pd.DataFrame(average_sequence, columns=["Average"])
+
+indices = []
+for i in range(n):
+    indices.append(i)
+df['index'] = indices
+
+sns.scatterplot(x='index', y='Average', data=df)
+plt.show()
+
+n = 1000
+sequence = generate_prime_reversal_sequence(n)
+
+average_sequence = []
+total = 0
+for i, s in enumerate(sequence):
+    total += s
+    average = total / (i + 1)
+    average_sequence.append(average)
+
+df = pd.DataFrame(average_sequence, columns=["Average"])
+
+indices = []
+for i in range(n):
+    indices.append(i)
+df['index'] = indices
+
+sns.scatterplot(x='index', y='Average', data=df)
+plt.show()
+
+n = 10000
+sequence = generate_prime_reversal_sequence(n)
+
+average_sequence = []
+total = 0
+for i, s in enumerate(sequence):
+    total += s
+    average = total / (i + 1)
+    average_sequence.append(average)
+
+df = pd.DataFrame(average_sequence, columns=["Average"])
+
+indices = []
+for i in range(n):
+    indices.append(i)
+df['index'] = indices
+
+sns.scatterplot(x='index', y='Average', data=df)
+plt.show()
+
+n = 100000
+sequence = generate_prime_reversal_sequence(n)
+
+average_sequence = []
+total = 0
+for i, s in enumerate(sequence):
+    total += s
+    average = total / (i + 1)
+    average_sequence.append(average)
+
+df = pd.DataFrame(average_sequence, columns=["Average"])
+
+indices = []
+for i in range(n):
+    indices.append(i)
+df['index'] = indices
+
+sns.scatterplot(x='index', y='Average', data=df)
+plt.show()
